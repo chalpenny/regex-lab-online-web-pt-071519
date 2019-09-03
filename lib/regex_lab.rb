828d50732a00012e45aaa2bf2d37d2c3ccdf7770
Colleen Halpenny <cp.halpenny@gmail.com>
2019-09-03 01:51:09 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,7 +1,9 @@
 require 'pry'
 
 def starts_with_a_vowel?(word)
-  word.match(/^[aeiouAEIOU]\w+/) ? true : false
+  if word.match(/^[aeiouAEIOU]\w+/) 
+    true 
+    else false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
